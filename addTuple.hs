c57cacfa6eca61e41db3c4d083eff05411eea829
Azahra Putri <azahra.putri@ui.ac.id>
2019-10-16 22:59:09 +0700

    Done addTuple

--- addTuple.hs
+++ addTuple.hs
@@ -5,6 +5,6 @@ addTuple (x,y) = x + y
 
 oneListOfTuples xs = map addTuple xs
 
-turnToList xss = map oneListOfTuples xss 
+turnToList xss = concat (map oneListOfTuples xss)
 
 main = print(turnToList [[(1,4),(1,5),(1,6)], [(2,4),(2,5),(2,6)]])
