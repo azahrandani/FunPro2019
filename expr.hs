51a34af16a61a63ad1dd643cc8384d095e6868d2
Azahra Putri <azahra.putri@ui.ac.id>
2019-10-18 21:14:55 +0700

    Test map of expr

--- expr.hs
+++ expr.hs
@@ -8,3 +8,7 @@ data Expr = C Float | Expr :+ Expr | Expr :- Expr
 exprMap :: (Expr -> Expr) -> [Expr] -> [Expr]
 exprMap f [] = []
 exprMap f (x:xs) = f x : exprMap f xs
+
+aFunction expr = expr :+ C 1
+
+main = print(exprMap aFunction [C 8, C 9, C 10])
