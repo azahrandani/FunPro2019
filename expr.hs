6d6543a2f6a93aad1e60cd71a3baed30a3cc08ea
Azahra Putri <azahra.putri@ui.ac.id>
2019-10-19 00:18:43 +0700

    Add foldr of Expr

--- expr.hs
+++ expr.hs
@@ -11,4 +11,8 @@ exprMap f (x:xs) = f x : exprMap f xs
 
 aFunction expr = expr :+ C 1
 
-main = print(exprMap aFunction [C 8, C 9, C 10])
+-- main = print(exprMap aFunction [C 8, C 9, C 10])
+
+exprFoldr :: (Expr -> Expr -> Expr) -> Expr -> [Expr] -> Expr
+exprFoldr op init [] = init
+exprFoldr op init (x:xs) = op x (exprFoldr op init xs)
