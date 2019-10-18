c419a57779ccab7414222f3d413859f6da68d8ba
Azahra Putri <azahra.putri@ui.ac.id>
2019-10-19 00:39:02 +0700

    Attempt #1 evaluate HOF

--- expr.hs
+++ expr.hs
@@ -19,4 +19,21 @@ exprFoldr op init (x:xs) = op x (exprFoldr op init xs)
 
 theFunction expr1 expr2 = expr1
 
-main = print(exprFoldr theFunction (C 0) [C 16, C 2, C 3, C 10])
+-- main = print(exprFoldr theFunction (C 0) [C 16, C 2, C 3, C 10])
+
+-- evaluate (C x) = x
+-- evaluate (e1 :+ e2) = evaluate e1 + evaluate e2
+-- evaluate (e1 :- e2) = evaluate e1 - evaluate e2
+-- evaluate (e1 :* e2) = evaluate e1 * evaluate e2
+-- evaluate (e1 :/ e2) = evaluate e1 / evaluate e2
+
+unConstant (C x) = x
+
+sum x y = x+y
+minus x y = x-y
+multiply x y = x*y
+divide x y = x/y
+
+evaluate (e1 :+ e2) = exprFoldr sum 0 (e1 : e2)
+
+main = print(evaluate(C 1 :+ C 2))
