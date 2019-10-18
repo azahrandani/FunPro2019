a7c3e77b391a4764f78d55417b41a877e4118f3e
Azahra Putri <azahra.putri@ui.ac.id>
2019-10-19 01:07:25 +0700

    Attempt #4 evaluate HOF

--- expr.hs
+++ expr.hs
@@ -30,13 +30,14 @@ theFunction expr1 expr2 = expr1
 unConstant :: Expr -> Float
 unConstant (C x) = x
 
-summ :: Expr -> Expr -> Expr
-summ e1 e2 = e1 :+ e2
+-- summ :: Expr -> Expr -> Expr
+summ e1 e2 = C (unConstant e1 + unConstant e2)
 
 minuss x y = x-y
 multiplyy x y = x*y
 dividee x y = x/y
 
-evaluate (e1 :+ e2) = unConstant(exprFoldr summ (C 0) (e1 : [e2]))
+-- evaluate (e1 :+ e2) = unConstant(exprFoldr summ (C 0) (e1 : [e2]))
 
-main = print(evaluate(C 1 :+ C 2))
+-- main = print(evaluate(C 1 :+ C 2))
+main = print(exprFoldr summ (C 0) [C 1, C 2])
